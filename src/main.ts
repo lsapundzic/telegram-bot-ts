517197d5c55f12c73117f9344cceaae4f89c054d
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2024-05-03 23:08:13 +0200

    listening to messages successful

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { Context, Telegraf } from 'telegraf'
-import { message } from 'telegraf/filters'
+import {Context, Telegraf} from 'telegraf'
+import {message} from 'telegraf/filters'
 
 const BOT_TOKEN: string = "6963748664:AAFBtwGm3Z8j3LN4FENWewp5jCpOf-yyZMU"
 
@@ -7,12 +7,10 @@ const bot = new Telegraf(BOT_TOKEN);
 
 console.log("---Program started---");
 
-bot.on(message('text'), async (ctx) => {
-  let a = ctx.message;
-    console.log(a.text);
-      
-    
-    await ctx.reply(`Hello friend <3`)
-  })
+bot.on(message('text'), async (context) => {
+    let messageMonitor = context.message;
+
+    await context.reply(`message received`)
+})
 
 bot.launch();
