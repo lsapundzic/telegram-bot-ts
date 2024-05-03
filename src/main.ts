979224c711765973169199e3bf46484e341f1353
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2024-05-03 22:46:40 +0200

    environment set

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-console.log("Hello :3");
+import { Context, Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters'
+
+const BOT_TOKEN: string = "6963748664:AAFBtwGm3Z8j3LN4FENWewp5jCpOf-yyZMU"
+
+const bot = new Telegraf(BOT_TOKEN);
+
+console.log("---Program started---");
+
+bot.on(message('text'), async (ctx) => {
+    
+  
+    // Using context shortcut
+    await ctx.reply(`Hello friend <3`)
+  })
+
+bot.launch();
