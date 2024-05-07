49f75206acc38e73b21029868f44267725e32969
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2024-05-07 17:32:57 +0200

    quickfix the token issue

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import {Context, Telegraf} from 'telegraf'
 import {message} from 'telegraf/filters'
 
-import * as dotenv from 'dotenv';
 
-dotenv.config();
+// Disabled npm package because idk how it works
+// import * as dotenv from 'dotenv';
 
-const token = process.env.BOT_TOKEN;
+// dotenv.config();
 
-const bot = new Telegraf(token);
+// const token = process.env.BOT_TOKEN;
+
+const BOT_TOKEN = "6963748664:AAHsTJkVqKa9ttyTBAF-J5T7g6p11NT0nZ8"
+
+const bot = new Telegraf(BOT_TOKEN);
 
 console.log("PROGRAM STARTED \n");
 
